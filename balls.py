2d94d7e892aac15f4055a017b69396eeb25dc73e
Vaalei <ville@tormanen.nu>
2022-04-07 21:10:21 +0200

    added resetabillity and needing resets to jumps

--- balls.py
+++ balls.py
@@ -7,6 +7,7 @@ import time
 
 class GameBase():
     def __init__(self, **args) -> None:
+        self.type = None
         super().__init__(**args)    
         self.vel = vec(0,0,0)  
         if "pos" not in args:
@@ -14,6 +15,9 @@ class GameBase():
         objects.append(self)
         self.startPos = self.pos
         
+    def rem(self):
+        self.visible = False
+        del self
 
     def mouseUp(self):
         pass
@@ -22,6 +26,9 @@ class GameBase():
         pass
 
     def update(self):
+        pass
+
+    def reset(self):
         pass
         
 
@@ -31,24 +38,33 @@ class Player(GameBase, sphere):
 
         
     def mouseUp(self):
+        if not self.canMove: return
         prelvec = self.pos-scene.mouse.pos
         self.vel=prelvec.norm()*self.force*self.charge
         self.charge = 0
         self.charging = False
+        self.slowmotion = False
+        
+        self.canMove = False
 
     
     def mouseDown(self):
+        if not self.canMove: return
         self.startCharge = time.time()
         self.charging = True
+        self.slowmotion = True
 
 
     def update(self):
         self.charge = time.time()-self.startCharge if time.time()-self.startCharge < self.chargeTimeLimit else self.chargeTimeLimit
-        chargeLabel.text = f"Charge: {self.charge:.2f}"
+        chargeLabel.text = f"Charge: {self.charge/self.chargeTimeLimit*100:.0f}%"
+
+        Trail(pos = self.pos, radius = self.radius/1.5, color = vec(0.9,0.9,0.9))
+
 
         lastCollision = None
         for obj in objects:
-            if self == obj:
+            if self == obj or not obj.type:
                 continue
             if detectCol(obj, self):
                 if lastCollision == obj:
@@ -56,17 +72,17 @@ class Player(GameBase, sphere):
                 if obj.type == "point":
                     normvec = norm(self.pos-obj.pos)
                     self.vel = mag(self.vel)*normvec*0.7
+                    self.gainPoint()
+                    obj.remove()
+                    self.canMove = True
+
                 elif obj.type == "platform":
                     self.vel = vec(0,1,0)
-                else:
-                    self.vel = -self.vel * 0.9
+
                 lastCollision = obj
         self.lastCollision = lastCollision     
         k = keysdown()
-        if "r" in k:
-            self.pos = vec(0,0,0)
-            self.vel = vec(0,0,0)
-        
+
         if not self.slowmotion:
             self.pos += self.vel
             self.vel.y -= self.gravitation 
@@ -77,20 +93,37 @@ class Player(GameBase, sphere):
             self.vel.x *= self.linearDrag * self.slowmoAmount
     
 
+    def gainPoint(self, amount = 1):
+        self.points += amount
+    
+
     charge = 0
     chargeTimeLimit = 1 # In seconds
     startCharge = 0
+    canMove = True
 
     force = 7
     jumpForce = 20
     gravitation = 0.1
     linearDrag = 1 # Percent
     
-    
+    points = 0 
     type = "player"
     slowmotion = False
     slowmoAmount = 0.3
-    
+    trail = []
+
+
+
+class Trail(GameBase, sphere):
+    def __init__(self, **args):
+        super().__init__(**args)
+        self.startRadius = self.radius
+
+    def update(self):
+        self.radius -= self.startRadius/20
+        if self.radius == 0:
+            self.rem(self)
 
     
 
@@ -102,7 +135,11 @@ class Point(GameBase, sphere):
         self.color = vec(0,1,0)
         self.radius = 7.5
         self.type = "point"
-        
+
+    def reset(self):
+        self.pos = vector.random()*300
+        self.pos.z = 0
+
 
 
 class Platform(GameBase, box):
@@ -130,41 +167,50 @@ def mouseUp():
 
 def mouseDown():
     for i in objects:
-        i.mouseDown()
+        i.mouseDown()    
+
+
+def update():
+    k = keysdown()
+    if "esc" in k: quitProg()
+    if "r" in k: reset()
+    for i in objects:
+        i.update()
+    
+
+def detectCol(obj1, obj2):
+    if isinstance(obj1, sphere) and isinstance(obj2, sphere):
+        objDistance = obj1.radius + obj2.radius
+        if mag(obj1.pos - obj2.pos) < objDistance:
+            return True
+    else:
+        xSize = (obj1.length + obj2.length)/2
+        ySize = (obj1.height + obj2.height)/2
+        zSize = (obj1.width + obj2.width)/2
+
+        currDist = obj1.pos - obj2.pos
+
+        xRange = abs(currDist.x) - xSize
+        yRange = abs(currDist.y) - ySize
+        zRange = abs(currDist.z) - zSize
+
+        if xRange <= 0 and yRange <= 0 and zRange <= 0:
+            return True
 
 
 def reset():
     for i in objects:
-        if i.type == "point":
-            del(i)
-    objects = []
+        i.reset()
     
+    start()
 
-
-def update():
-    for i in objects:
-        i.update()
-
-
-def detectCol(obj1, obj2):
-    xSize = (obj1.length + obj2.length)/2
-    ySize = (obj1.height + obj2.height)/2
-    zSize = (obj1.width + obj2.width)/2
-
-    currDist = obj1.pos - obj2.pos
-
-    xRange = abs(currDist.x) - xSize
-    yRange = abs(currDist.y) - ySize
-    zRange = abs(currDist.z) - zSize
-
-    if xRange <= 0 and yRange <= 0 and zRange <= 0:
-        return True
 
 def start():
     global objects, player, pointList, platformList
     objects = []
     pointList = []
     platformList = []
+    
     player = Player(pos=vec(0,0,0), radius = 15, color=vec(0,0,1))
     
     Platform(pos=vec(0,-150,0), color = vec(0,0,1))
@@ -183,15 +229,15 @@ scene.background = vec(0.1,0.1,0.1)
 
 scene.bind("mousedown", mouseDown)
 scene.bind("mouseup", mouseUp)
-scene.bind("escape", quitProg)
+#scene.bind("esc", quitProg)
 
-chargeLabel = label(pos=vec(-370,200,0), text="Charge: 0", height=40, box = False, color = vec(1,0,0), font='sans')
-
+chargeLabel = label(pos=vec(-370,200,0), text="Charge: 0%", height=40, box = False, color = vec(1,0,0), font='sans')
 
 
 while True:
     rate(60)
     update()
+    
 
 
 
